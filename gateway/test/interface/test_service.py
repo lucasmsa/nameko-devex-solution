70354201fc1990cc6130af38c95a2d7890b4f555
Lucas Moreira <lucasmsea@outlook.com>
2023-04-25 15:29:17 -0300

    test: adjust tests for the products service

--- gateway/test/interface/test_service.py
+++ gateway/test/interface/test_service.py
@@ -53,6 +53,7 @@ class TestCreateProduct(object):
         )
         assert response.status_code == 200
         assert response.json() == {'id': 'the_odyssey'}
+        
         assert gateway_service.products_rpc.create.call_args_list == [call({
                 "in_stock": 10,
                 "maximum_speed": 5,
@@ -60,7 +61,7 @@ class TestCreateProduct(object):
                 "passenger_capacity": 101,
                 "title": "The Odyssey"
             })]
-
+        
     def test_create_product_fails_with_invalid_json(
         self, gateway_service, web_session
     ):
@@ -80,6 +81,23 @@ class TestCreateProduct(object):
         assert response.status_code == 400
         assert response.json()['error'] == 'VALIDATION_ERROR'
 
+class TestDeleteProduct(object):
+    def test_can_delete_product(self, gateway_service, web_session):
+        response = web_session.delete('/products/the_odyssey')
+        assert response.status_code == 204
+        assert gateway_service.products_rpc.delete.call_args_list == [
+            call("the_odyssey")
+        ]
+
+    def test_product_not_found_on_delete(self, gateway_service, web_session):
+        gateway_service.products_rpc.delete.side_effect = (
+            ProductNotFound('missing'))
+
+        response = web_session.delete('/products/foo')
+        assert response.status_code == 404
+        payload = response.json()
+        assert payload['error'] == 'PRODUCT_NOT_FOUND'
+        assert payload['message'] == 'missing'
 
 class TestGetOrder(object):
 
--- test/nex-smoketest.sh
+++ test/nex-smoketest.sh
@@ -34,6 +34,16 @@ curl -s -XPOST  "${STD_APP_URL}/products" \
     -H 'Content-Type: application/json' \
     -d '{"id": "the_odyssey", "title": "The Odyssey", "passenger_capacity": 101, "maximum_speed": 5, "in_stock": 10}'
 echo
+
+# Test: Get Product
+echo "=== Getting product id: the_odyssey ==="
+curl -s "${STD_APP_URL}/products/the_odyssey" | jq .
+
+# Test: Delete Product
+echo "=== Deleting product id: the_odyssey ==="
+curl -s -XDELETE "${STD_APP_URL}/products/the_odyssey"
+echo "Product deleted"
+
 # Test: Get Product
 echo "=== Getting product id: the_odyssey ==="
 curl -s "${STD_APP_URL}/products/the_odyssey" | jq .
